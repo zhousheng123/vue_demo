0a535a3b784409847c0e430b5f520c205490ed97
zhousheng123 <1040050116@qq.com>
2019-01-21 10:22:07 +0800

    zzz

---
+++ zhouzhou/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import auth from '@/components/Auth'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/auth',
+      name: 'Auth',
+      component: auth
+    }
+
+  ]
+})
